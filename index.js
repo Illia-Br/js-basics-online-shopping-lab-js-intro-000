e88fd5dcc06a8d5bc2678eeec124603ece7caf6a
Illia Brytun <illiabrytun@gmail.com>
2020-04-13 18:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,37 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemDescription = {itemName: item, itemPrice: Math.floor(Math.random()*100)+1}
+ cart.push(itemDescription)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length===0) {
+    return 'Your shopping cart is empty.'
+  }
+  var itemsAtPrice = []
+  for (let i=0; i<cart.length; i++) {
+    itemsAtPrice.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  if (itemsAtPrice.length === 1) {
+    return `In your cart, you have ${itemsAtPrice}.`
+  }
+  itemsAtPrice[itemsAtPrice.length-1] = `and ${itemsAtPrice[itemsAtPrice.length-1]}`
+  return `In your cart, you have ${itemsAtPrice.join(', ')}.`
 }
+
 
 function total() {
   // write your code here
+  var total=0
+  for (let i=0; i<cart.length; i++) {
+    total = parseInt(cart[i].itemPrice, 10) + total
+  }
+  return total
 }
+
 
 function removeFromCart(item) {
   // write your code here
